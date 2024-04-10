c8c8c177b6383c934e3f84e17807c35efec54c0d
Collins Kariuki <cmka2020@mymail.pomona.edu>
2024-04-09 20:51:01 -0700

    Add app name & add event button

--- app/src/main/java/com/example/countdown_timer_app/MainActivity.kt
+++ app/src/main/java/com/example/countdown_timer_app/MainActivity.kt
@@ -3,13 +3,27 @@ package com.example.countdown_timer_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.countdown_timer_app.ui.theme.CountdowntimerappTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeScreenLayout( onAddEventClicked = { })
                 }
             }
         }
@@ -30,17 +44,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomeScreenLayout(onAddEventClicked: () -> Unit) {
+    Column (
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Countdown Timer App",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            textAlign = TextAlign.Center)
+
+        Spacer(modifier = Modifier.height(48.dp))
+
+        Button(
+            onClick = { onAddEventClicked() },
+            modifier = Modifier.fillMaxWidth().height(50.dp),
+            shape = RoundedCornerShape(8.dp),
+            border = BorderStroke(1.dp, Color.Black)
+        ) {
+            Text(
+                text = "Add a new event",
+                fontSize = 18.sp,
+                color = Color.Black
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CountdowntimerappTheme {
-        Greeting("Android")
+        HomeScreenLayout(onAddEventClicked = { })
     }
 }
