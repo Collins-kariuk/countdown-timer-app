ad35c9929b91d178333fabe4c1a2501db5040d37
Collins Kariuki <cmka2020@mymail.pomona.edu>
2024-05-31 15:49:45 -0700

    Add necessary dependency for Jetpack Compose Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,8 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
