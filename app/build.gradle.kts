1053eb698e21f0fdf8f9a5c552c8e07ee9f6ba57
Collins Kariuki <cmka2020@mymail.pomona.edu>
2024-06-04 11:15:25 -0700

    Add Room Database dependencies (I hope I fixed it)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -62,6 +63,11 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.2.1")
     implementation("com.google.ar.sceneform:filament-android:1.17.1")
     implementation("androidx.wear.compose:compose-material:1.3.1")
+
+    // Implementations for the Room database.
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
