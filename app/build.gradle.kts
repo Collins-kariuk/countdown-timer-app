a8245762715587fb1430e33d8cb9bbe6b2aa89a4
Collins Kariuki <cmka2020@mymail.pomona.edu>
2024-06-11 14:30:31 -0700

    BRO! All I had to do was hover over version #s to update them!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,22 +54,18 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material3:material3:1.2.1")
-    implementation("com.google.ar.sceneform:filament-android:1.17.1")
-    implementation("androidx.wear.compose:compose-material:1.3.1")
-    implementation("androidx.compose.ui:ui-text-google-fonts:1.6.7")
-    implementation("com.google.android.datatransport:transport-api:3.2.0")
-
-    // Implementations for the Room database.
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.room:room-runtime:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.wear.compose:compose-material:1.3.1") // Add this line
+    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.7")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
